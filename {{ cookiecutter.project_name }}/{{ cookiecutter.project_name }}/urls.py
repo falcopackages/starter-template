b0092564950147e440f5d53c1cfbe5a0d6b9f8e3
Tobi DEGNON <tobidegnon@proton.me>
2024-09-13 10:03:50 +0100

    refactor: migrate to new falco app

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
@@ -5,7 +5,8 @@ from django.urls import include
 from django.urls import path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
-from falco_toolbox import views as toolbox_views
+from falco import views as falco_views
+from falco.urls import errors_views
 from health_check.views import MainView
 from allauth.account.decorators import secure_admin_login
 
@@ -13,9 +14,9 @@ admin.autodiscover()
 admin.site.login = secure_admin_login(admin.site.login)
 
 urlpatterns = [
-    path(".well-known/security.txt", toolbox_views.security_txt),
-    path("robots.txt", toolbox_views.robots_txt),
-    path("", include("falco_ui.favicons.urls")),
+    path(".well-known/security.txt", falco_views.security_txt),
+    path("robots.txt", falco_views.robots_txt),
+    path("", include("falco.favicons.urls")),
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "about/",
@@ -31,21 +32,6 @@ if settings.DEBUG:
     urlpatterns += [
         path("__reload__/", include("django_browser_reload.urls")),
         path("__debug__/", include("debug_toolbar.urls")),
-        path(
-            "400/",
-            default_views.bad_request,
-            kwargs={"exception": Exception("Bad Request!")},
-        ),
-        path(
-            "403/",
-            default_views.permission_denied,
-            kwargs={"exception": Exception("Permission Denied")},
-        ),
-        path(
-            "404/",
-            default_views.page_not_found,
-            kwargs={"exception": Exception("Page not Found")},
-        ),
-        path("500/", default_views.server_error),
+        *errors_views,
         *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     ]
