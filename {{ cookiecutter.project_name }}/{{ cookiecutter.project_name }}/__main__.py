05cdbe2f83e09a2feb46730e42bf0d0a51a133b7
Tobi DEGNON <tobidegnon@proton.me>
2024-10-08 16:29:58 +0100

    auto use of litestream when sqlite is used

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__main__.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__main__.py
@@ -1,17 +1,29 @@
-def main() -> None:
-    import os
-    import sys
-    from pathlib import Path
+import os
+import sys
+import multiprocessing
+import subprocess
+from pathlib import Path
+from typing import List
+from contextlib import suppress
 
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_name }}.settings"
-    )
-    current_path = Path(__file__).parent.parent.resolve()
-    sys.path.append(str(current_path))
+HOST = "0.0.0.0"
+PORT = 8000
+PROJECT_NAME = "{{ cookiecutter.project_name }}"
+WSGI_APPLICATION = f"{PROJECT_NAME}.wsgi:application"
 
-    run_func = None
-    if len(sys.argv) > 1:
-        run_func = COMMANDS.get(sys.argv[1])
+current_path = Path(__file__).parent.parent.resolve()
+sys.path.append(str(current_path))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{PROJECT_NAME}.settings")
+
+
+def main():
+    """
+    Entry point of the script. Determines which command to execute based on CLI arguments.
+    """
+    # Retrieve the command key from the first CLI argument, if present
+    command_key = sys.argv[1] if len(sys.argv) > 1 else None
+    run_func = COMMANDS.get(command_key)
 
     if run_func:
         run_func(sys.argv)
@@ -20,10 +32,11 @@ def main() -> None:
 
 
 def run_setup(_):
-    """Run some project setup tasks"""
+    """
+    Run project setup tasks, applying migrations, creating a superuser if needed etc.
+    """
     from django.core.management import execute_from_command_line
     from django.core.management.base import CommandError
-    from contextlib import suppress
 
     execute_from_command_line(["manage", "litestream", "init"])
     execute_from_command_line(
@@ -38,48 +51,56 @@ def run_setup(_):
     )
     execute_from_command_line(["manage", "migrate"])
     execute_from_command_line(["manage", "setup_periodic_tasks"])
-
     with suppress(CommandError):
         execute_from_command_line(
             ["manage", "createsuperuser", "--noinput", "--traceback"]
         )
 
 
-def run_granian(argv: list) -> None:
+def run_granian(_):
     """
-    Run granian the WSGI server.
-    https://github.com/emmett-framework/granian
+    Run Granian, the WSGI server. If using SQLite as the database, Litestream is used for replication.
     """
-    import multiprocessing
-    import granian
-    from granian.constants import Interfaces
+    bin_dir = Path(sys.executable).parent
+    granian_executable = bin_dir / "granian"
 
     workers = multiprocessing.cpu_count() * 2 + 1
-    granian.Granian(
-        "{{ cookiecutter.project_name }}.wsgi:application",
-        interface=Interfaces.WSGI,
-        workers=workers,
-        address="0.0.0.0",
-        port=8000,
-    ).serve()
+    granian_args = [
+        "--workers",
+        str(workers),
+        "--interface",
+        "wsgi",
+        "--port",
+        str(PORT),
+        "--host",
+        HOST,
+        WSGI_APPLICATION,
+    ]
+
+    if "sqlite" in os.getenv("DATABASE_URL", ""):
+        # Construct the Granian command as a single string for Litestream
+        litestream_executable = bin_dir / "litestream"
+        execute_granian = f"granian {' '.join(granian_args)}"
+        command = [str(litestream_executable), "replicate", "-exec", execute_granian]
+    else:
+        command = [str(granian_executable)] + granian_args
+
+    subprocess.run(command, check=True)
 
 
-def run_litestream(argv: list) -> None:
-    """Run Litestream via django-litestream."""
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(["manage", "litestream", *argv[2:]])
-
-
-def run_qcluster(argv: list) -> None:
-    """Run Django-q cluster."""
+def run_qcluster(argv: List[str]) -> None:
+    """
+    Run the Django Q cluster for handling background tasks.
+    """
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(["manage", "qcluster", *argv[2:]])
 
 
-def run_manage(argv: list) -> None:
-    """Run Django's manage command."""
+def run_manage(argv: List[str]) -> None:
+    """
+    Run Django's manage.py commands.
+    """
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(argv[1:])
@@ -89,7 +110,6 @@ COMMANDS = {
     "qcluster": run_qcluster,
     "manage": run_manage,
     "setup": run_setup,
-    "litestream": run_litestream,
 }
 
 if __name__ == "__main__":
