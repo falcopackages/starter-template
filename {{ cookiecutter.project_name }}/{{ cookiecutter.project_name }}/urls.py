c462f3c5b606b1075c41bb422922183ed12bf180
Tobi DEGNON <tobidegnon@proton.me>
2024-10-13 12:08:48 +0100

    refactor urls to match new falco version

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
@@ -5,7 +5,7 @@ from django.urls import include
 from django.urls import path
 from django.views.generic import TemplateView
 from falco import views as falco_views
-from falco.urls import errors_views
+from falco.urls import favicon_urlpatterns, errors_urlpatterns
 from health_check.views import MainView
 from allauth.account.decorators import secure_admin_login
 
@@ -15,7 +15,7 @@ admin.site.login = secure_admin_login(admin.site.login)
 urlpatterns = [
     path(".well-known/security.txt", falco_views.security_txt),
     path("robots.txt", falco_views.robots_txt),
-    path("", include("falco_ui.urls")),
+    path("", include(favicon_urlpatterns)),
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path("health/", MainView.as_view()),
     path("accounts/", include("allauth.urls")),
@@ -26,6 +26,6 @@ if settings.DEBUG:
     urlpatterns += [
         path("__reload__/", include("django_browser_reload.urls")),
         path("__debug__/", include("debug_toolbar.urls")),
-        *errors_views,
         *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     ]
+    urlpatterns += errors_urlpatterns
