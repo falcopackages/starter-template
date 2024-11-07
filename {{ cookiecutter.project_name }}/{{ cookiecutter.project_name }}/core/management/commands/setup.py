138faad18f8536be7519fbfddca44b7e5ec5c138
Tobi DEGNON <tobidegnon@proton.me>
2024-11-07 11:27:35 +0100

    litestream can be disabled via env

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/core/management/commands/setup.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/core/management/commands/setup.py
@@ -1,22 +1,26 @@
 from contextlib import suppress
 
 from django.core.management import call_command
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
+
+from .prodserver import use_litestream
 
 
 class Command(BaseCommand):
     help = "Run project setup tasks, applying migrations, creating a superuser if needed etc."
 
     def handle(self, *_, **__):
-        call_command("litestream", "--skip-checks", "init")
-        call_command(
-            "litestream",
-            "--skip-checks",
-            "restore",
-            "default",
-            "-if-replica-exists",
-            "-if-db-not-exists",
-        )
+        if use_litestream():
+            call_command("litestream", "--skip-checks", "init")
+            call_command(
+                "litestream",
+                "--skip-checks",
+                "restore",
+                "default",
+                "-if-replica-exists",
+                "-if-db-not-exists",
+            )
         call_command("migrate", "--skip-checks")
         call_command("setup_periodic_tasks", "--skip-checks")
         with suppress(CommandError):
